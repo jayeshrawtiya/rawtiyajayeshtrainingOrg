b1c3470260e3206cfb03ef0e60b577458add73b7
Jayesh Rawtiya <rawtiyajayesh@gmail.com>
2025-12-27 23:36:02 +0530

    News App

--- force-app/main/default/lwc/NewsApp/newsApp.js
+++ force-app/main/default/lwc/NewsApp/newsApp.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class NewsApp extends LightningElement {}
+export default class NewsApp extends LightningElement {
+    sortByOptions = [
+        { label: 'Relevance', value: 'relevance' },
+        { label: 'Popularity', value: 'popularity' },
+        { label: 'Published At', value: 'publishedAt' }
+    ];
+}
